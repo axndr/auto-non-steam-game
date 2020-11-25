9d329ce1d69f5a7ca9c107c5b7b014cb5816b956
Alex Clifford <alex.clifford117@gmail.com>
2020-11-25 12:41:24 -0600

    _shortcut_generator.py is now working

--- main.py
+++ main.py
@@ -5,28 +5,14 @@ import os
 import re
 
 
-"""
-'\x00\x01
-AppName\x00Audacity\x00\x01
-Exe\x00"C:\\Program Files (x86)\\Audacity\\audacity.exe"\x00\x01
-StartDir\x00"C:\\Program Files (x86)\\Audacity\\"\x00\x01
-icon\x00\x00\x01
-ShortcutPath\x00\x00\x01
-LaunchOptions\x00\x00\x02
-IsHidden\x00\x00\x00\x00\x00\x02
-AllowDesktopConfig\x00\x01\x00\x00\x00\x02
-AllowOverlay\x00\x01\x00\x00\x00\x02
-OpenVR\x00\x00\x00\x00\x00\x02
-Devkit\x00\x00\x00\x00\x00\x01
-DevkitGameID\x00\x00\x02
-LastPlayTime\x00_\x1d¸_\x00
-tags\x00'
-
-"""
-
 if __name__ == '__main__':
     steam_obj = Steam("C:\\Program Files (x86)\\Steam\\userdata")
     localusercontext_obj = LocalUserContext(steam_obj, 76561197993493993)
     alex = User(localusercontext_obj.steam, localusercontext_obj.user_id)
-
-    pass
+    new_shortcut = Shortcut(
+        AppName='Breach',
+        Exe=r"C:\Games\Into.the.Breach.v1.2.24\Breach.exe",
+        StartDir=r"C:\Games\Into.the.Breach.v1.2.24"
+    )
+    alex.shortcuts.append(new_shortcut)
+    alex.save_shortcuts()
--- model.py
+++ model.py
@@ -1,35 +1,36 @@
-
 import collections
 
-# Represents a single shortcut on the user's machine. These are the 5 fields
-# which are stored in shortcuts.vdf.
-Shortcut = collections.namedtuple('Shortcut', [
-  'AppName',
-  'Exe',
-  'StartDir',
-  'icon',
-  'ShortcutPath',
-  'LaunchOptions',
-  'IsHidden',
-  'AllowDesktopConfig',
-  'AllowOverlay',
-  'OpenVR',
-  'Devkit',
-  'DevkitGameID',
-  'LastPlayTime',
-  'tags',
-])
+x00 = u'\x00'
+x01 = u'\x01'
 
+
+class Shortcut:
+    def __init__(self, AppName, Exe, StartDir, ShortcutPath='', LaunchOptions='',
+                 DevkitGameID='', LastPlayTime=x00 + x00 + x00 + x00, tags=[], icon='', IsHidden=x00,
+                 AllowDesktopConfig=x00,
+                 AllowOverlay=x00, OpenVR=x00, Devkit=x00):
+        self.AppName = AppName
+        self.Exe = Exe
+        self.StartDir = StartDir
+        self.icon = icon
+        self.ShortcutPath = ShortcutPath
+        self.LaunchOptions = LaunchOptions
+        self.IsHidden = IsHidden
+        self.AllowDesktopConfig = AllowDesktopConfig
+        self.AllowOverlay = AllowOverlay
+        self.OpenVR = OpenVR
+        self.Devkit = Devkit
+        self.DevkitGameID = DevkitGameID
+        self.LastPlayTime = LastPlayTime
+        self.tags = tags
 
 
 # Represents a Steam installation. Since we don't really care about where the
 # actual guts of Steam are located, the only property on this object is the
 # location of the userdata directory (much more interesting).
 Steam = collections.namedtuple('Steam', [
-  'userdata_directory',
+    'userdata_directory',
 ])
-
-
 
 # A simple composite object that encapsulates a local steam installation
 # with a user id. Since basically everything that you would want pysteam to do
@@ -37,8 +38,6 @@ Steam = collections.namedtuple('Steam', [
 # shortcuts, etc), most functions take this as a parameter so they dont need to
 # take both a Steam installation and a user id.
 LocalUserContext = collections.namedtuple('LocalUserContext', [
-  'steam',
-  'user_id',
+    'steam',
+    'user_id',
 ])
-
-
